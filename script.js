8e0f8dbf04d25e9ad6cd726f0ec90deda2758421
jur42282 <jur42282@spseol.cz>
2024-03-23 12:08:42 +0100

    added FAQ

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function showSidebar() {
   const sidebar =document.querySelector('.sidebar');
   sidebar.style.display = 'flex';
 }
+
+const question = document.querySelectorAll('.question');
+question.forEach((q) => {
+  q.addEventListener('click', () => {
+    q.classList.toggle('active');
+  });
+});
