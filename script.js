ae4a562d19cda7f919b16b2a744eaa128991c177
jur42282 <jur42282@spseol.cz>
2024-04-08 21:24:22 +0200

    +Aktuality

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ question.forEach((q) => {
     q.classList.toggle('active');
   });
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
